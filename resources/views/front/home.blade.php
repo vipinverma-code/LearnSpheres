4cae0fc82ae309167cedbfab668f455e13bb54ef
Vipin Verma <vipin@weblytechnolab.com>
2025-09-06 17:56:16 +0530

    changes home page

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,4 +1,10 @@
 @extends('front.layout.master')
 @section('content')
-    @include('front.components.course-categories')
+    {{-- @include('front.components.course-categories') --}}
+    @include('front.components.mainbanner')
+    @include('front.components.counterstart')
+    @include('front.components.popularcourse')
+    @include('front.components.actionbox')
+    @include('front.components.trendingcourse')
+    @include('front.components.reveiws')
 @endsection
