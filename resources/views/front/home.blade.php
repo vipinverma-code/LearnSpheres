59d907c639bd05cb8c3659377fcf5f6a56968fed
Abhishek Singh <148897811+Abhi7251singh@users.noreply.github.com>
2025-09-06 17:24:01 +0530

    new changes

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,5 +1,4 @@
 @extends('front.layout.master')
 @section('content')
     @include('front.components.course-categories')
-  
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::view('/','front.home');
-Route::view('/home','front.about_us');
-Route::view('/coursedetailsvideo','front.course_detailsvideo');
-Route::view('/courselist','front.course-list-2');
-Route::view('/SignIn','front.signin');
-Route::view('/SignUp','front.signup_page');
+Route::view('/', 'front.home');
+Route::view('/about', 'front.about_us');
+Route::view('/coursedetailsvideo', 'front.course_detailsvideo');
+Route::view('/courselist', 'front.course-list-2');
+Route::view('/SignIn', 'front.signin');
+Route::view('/SignUp', 'front.signup_page');
